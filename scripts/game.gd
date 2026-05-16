4681366344b321eef0d240d9a8993ca782ee89cc
ABHILESH SINGH <sabhilesh260@gmail.com>
2026-05-17 04:44:11 +0530

    added the score label now we can count how many times the user have clicke d on the button

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,5 +1,8 @@
 extends Control
 @onready var square: Button = $square
+@onready var score_label: Label = $score_label
+
+var score = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,12 +16,16 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	# 1. Get the width and height of the actual game window
-	var window_size = get_viewport_rect().size
+	# 1. Increase the score by 1
+	score += 1
 	
-	# 2. Pick a random X and Y position within that window
+	# 2. Update the ScoreLabel text to show the new score
+	$score_label.text = "Score: " + str(score)
+	
+	# 3. Pick a random X and Y position within the window
+	var window_size = get_viewport_rect().size
 	var random_x = randf_range(0, window_size.x - 128);
 	var random_y = randf_range(0, window_size.y - 128);
 	
-	# 3. Move the square to that new position
+	# 4. Move the square to that new position
 	$square.global_position = Vector2(random_x, random_y)
