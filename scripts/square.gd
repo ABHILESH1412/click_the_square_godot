7bbf34a377ddb314a1254f7f43d6754c0c909485
ABHILESH SINGH <sabhilesh260@gmail.com>
2026-05-17 05:28:20 +0530

    successfully created the area2d node and made it clickable, on clicking it is moving to random positions too

---
+++ scripts/square.gd
@@ -0,0 +1,19 @@
+extends Area2D
+@onready var square: Area2D = $"."
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	# Check if the event is a mouse button press and if it's the LEFT click
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:		
+		# 1. Get the size of the game window
+		var window_size = get_viewport_rect().size
+		
+		# 2. Pick a random position
+		# (Godot icon is 128x128. Because Area2D positions are calculated from the CENTER of the sprite,
+		# we keep it 64 pixels away from the edges so it doesn't clip out of bounds)
+		var random_x = randf_range(64, window_size.x - 64)
+		var random_y = randf_range(64, window_size.y - 64)
+		
+		# 3. Move the Area2D square to the new position
+		$".".position = Vector2(random_x, random_y)
+		#$square.position = Vector2(random_x, random_y)
