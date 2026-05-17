4f79ce8347efc718e91e2ef15544e1cd2e31d201
ABHILESH SINGH <sabhilesh260@gmail.com>
2026-05-17 05:35:12 +0530

    added the score label

--- scripts/square.gd
+++ scripts/square.gd
@@ -1,10 +1,14 @@
 extends Area2D
-@onready var square: Area2D = $"."
+var score: int = 0
+@onready var score_label: Label = $"../score_label"
 
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	# Check if the event is a mouse button press and if it's the LEFT click
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:		
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		score += 1
+		score_label.text = "Score: " + str(score)
+		
 		# 1. Get the size of the game window
 		var window_size = get_viewport_rect().size
 		
@@ -15,5 +19,5 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 		var random_y = randf_range(64, window_size.y - 64)
 		
 		# 3. Move the Area2D square to the new position
-		$".".position = Vector2(random_x, random_y)
+		position = Vector2(random_x, random_y)
 		#$square.position = Vector2(random_x, random_y)
